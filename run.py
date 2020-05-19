c1fc68c870defed02cfcea59ef3c14986f740b3a
Samantha Simmons <sammysimmons274@gmail.com>
2020-05-19 21:15:13 +0100

    commit

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 import os
-#Capital F indicates class name, 
 from flask import Flask, render_template
 
-#this is a variable that we have stored in app
 app = Flask(__name__)
 
-#route decorator in python decorater start with @
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -25,11 +23,8 @@ def contact():
 def careers():
     return render_template("careers.html")
 
-#reference built in variable, if name is equal to main which is default module in python
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-         port=int(os.environ.get('PORT', 8000)),
+            port=int(os.environ.get('PORT')),
             debug=True)
-
-            #enviro.get is a set template
-            #debug should never be set to true only good for testing, as security flaw
