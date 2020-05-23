c3135ab91895dd255693d0a97cfd79b6d1481792
Samantha Simmons <sammysimmons274@gmail.com>
2020-05-23 16:09:33 +0100

    testing list notations within about page

--- run.py
+++ run.py
@@ -11,20 +11,20 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About", list_of_numbers=[1,2,3])
 
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route('/careers')
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT', 8000)),
+           app.run(host=os.environ.get('IP', '127.0.0.1'),              
+           port=int(os.environ.get('PORT', 5000)),
             debug=True)
